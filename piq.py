667009dbb5fd3ce3c967cfb4fb873b7f67a2de36
hdznrrd <github@follvalsch.de>
2014-07-30 12:14:46 +0200

    CLI boilerplate for piq

---
+++ piq.py
@@ -0,0 +1,22 @@
+"""\
+Usage:  piq.py dump [-o OFFSET] [-f FRAMES] FILE
+        piq.py find [-o OFFSET] [-f FRAMES] PATTERN FILE
+
+Arguments:
+        FILE        Input file in 16 bit I/Q format
+        PATTERN     Pattern in 16 bit I/Q format to be searched in FILE
+
+Options:
+        -h --help   Show this help message and exit
+        -o OFFSET   Number of frames to skip before processing [default: 0]
+        -f FRAMES   Limit number of frames to process (at most)
+"""
+
+from docopt import docopt
+
+def main():
+    """Entry point"""
+    arguments = docopt(__doc__)
+
+if __name__ == '__main__':
+    main()
