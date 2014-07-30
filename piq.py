c956be4e332114e8d050f629edc80fd13c11e21e
hdznrrd <github@follvalsch.de>
2014-07-30 12:32:38 +0200

    added dispatcher function and opening of wave file using wave module

--- piq.py
+++ piq.py
@@ -13,7 +13,7 @@ Options:
 """
 
 from docopt import docopt
-
+import wave
 
 class Piq(object):
     """Application class for piq"""
@@ -28,13 +28,33 @@ class Piq(object):
         """Find a pattern within another file"""
         pass
 
+    def dispatch(self):
+        """Dispatcher for the command interface"""
+        if self.arguments['dump']:
+            try:
+                self.haystack_file = wave.open(self.arguments['FILE'], 'rb')
+                self.do_dump()
+            finally:
+                if self.haystack_file:
+                    self.haystack_file.close()
+
+        elif self.arguments['find']:
+            try:
+                self.needle_file = wave.open(self.arguments['PATTERN'], 'rb')
+                self.haystack_file = wave.open(self.arguments['FILE'], 'rb')
+                self.do_find()
+            finally:
+                if self.needle_file:
+                    self.needle_file.close()
+                if self.haystack_file:
+                    self.haystack_file.close()
+
     def run(self):
         """Entry point of the application"""
-        if self.arguments['dump']:
-            self.do_dump()
-        elif self.arguments['find']:
-            self.do_find()
-
+        try:
+            self.dispatch()
+        except:
+            pass
 
 if __name__ == '__main__':
     Piq(docopt(__doc__)).run()
